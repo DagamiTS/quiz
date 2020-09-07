96d8591e7ce8eb702dcacd66cfd0101056f384b0
Denis Efremov <dagamitheslayer@gmail.com>
2020-09-07 17:25:53 +0300

    Created a simple quiz using React and TypeScript

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+
+type Props = {
+  question: string;
+  answers: Array<string>;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCart: React.FC<Props> = ({
+                                         question,
+                                         answers,
+                                         callback,
+                                         userAnswer,
+                                         questionNr,
+                                         totalQuestions
+                                       }) => {
+  // @ts-ignore
+  return (
+    <Wrapper>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map(answer => (
+          <ButtonWrapper
+            key={answer}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </ButtonWrapper>
+        ))}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default QuestionCart;
