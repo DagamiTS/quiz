5ee31ae8a8c4118e75c439e2e58a43ab5038a7d5
Denis Efremov <dagamitheslayer@gmail.com>
2020-09-07 17:19:31 +0300

    Updates

---
+++ precache-manifest.cf920a3b89d11955a8bd302f5601cbd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff994d8542547042b6cd18340e1563f",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "65cec92d19698c863c67",
+    "url": "/quiz/static/js/2.4b1d2a92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz/static/js/2.4b1d2a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6012ff09cb41e46a80d3",
+    "url": "/quiz/static/js/main.bdaf6bbd.chunk.js"
+  },
+  {
+    "revision": "a7407dc1a44539dae5d4",
+    "url": "/quiz/static/js/runtime-main.ac6f8894.js"
+  },
+  {
+    "revision": "b329823379c93e19725b70ca1d95d369",
+    "url": "/quiz/static/media/bg.b3298233.jpg"
+  }
+]);
